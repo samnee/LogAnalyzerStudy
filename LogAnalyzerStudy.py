cc02d297d6a8fcc49eaa8687e43ffdbf4ab915cb
nzg <nizugao001@163.com>
2017-09-25 17:38:26 +0800

    获取了绝对路径。

--- LogAnalyzerStudy.py
+++ LogAnalyzerStudy.py
@@ -1,5 +1,10 @@
 import os
 def main():    	print "study LogAnalyzer"
+	
+	dirName = os.path.dirname(os.path.abspath(__file__))
+	
+	print dirName
+	# D:\NZG\adp\python
 
 if __name__ == "__main__":
     main()
